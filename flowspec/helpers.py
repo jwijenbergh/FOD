17ff3ff9a2ce523b5496a8bfcb15661d54d8efbf
David Schmitz <schmitz@lrz.de>
2018-05-29 12:28:52 +0000

    added exception handling around failing email sending

--- flowspec/helpers.py
+++ flowspec/helpers.py
@@ -1,10 +1,13 @@
 from django.core.mail.message import EmailMessage
 from django.conf import settings
 
+import os
 
 def send_new_mail(subject, message, from_email, recipient_list, bcc_list):
+  try:
     return EmailMessage(subject, message, from_email, recipient_list, bcc_list).send()
-
+  except Exception, e:
+    os.write(3, "send_new_mail() failed: exc="+str(e)+"\n") # TODO use propper logging
 
 def get_peer_techc_mails(user, peer):
     mail = []
