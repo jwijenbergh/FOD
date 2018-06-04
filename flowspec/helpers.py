594ff9252b9cb816022ba960582704ec4a6586d5
David Schmitz <schmitz@lrz.de>
2018-06-04 09:03:48 +0000

    add log message for send mail function

--- flowspec/helpers.py
+++ flowspec/helpers.py
@@ -2,14 +2,23 @@ from django.core.mail.message import EmailMessage
 from django.conf import settings
 
 import os
+import logging
+
+FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 def send_new_mail(subject, message, from_email, recipient_list, bcc_list):
   try:
+    logger.info("helpers::send_new_mail(): send mail: from_email="+str(from_email)+", recipient_list="+str(recipient_list)+", bcc_list="+str(bcc_list)) 
     return EmailMessage(subject, message, from_email, recipient_list, bcc_list).send()
   except Exception, e:
-    os.write(3, "send_new_mail() failed: exc="+str(e)+"\n") # TODO use propper logging
+    #os.write(3, "send_new_mail() failed: exc="+str(e)+"\n") 
+    logger.error("helpers::send_new_mail() failed: exc="+str(e)) 
 
 def get_peer_techc_mails(user, peer):
+    logger.info("helpers::get_peer_techc_mails(): user="+str(user)+", peer="+str(peer))
     mail = []
     additional_mail = []
     techmails_list = []
@@ -25,4 +34,6 @@ def get_peer_techc_mails(user, peer):
         additional_mail = settings.NOTIFY_ADMIN_MAILS
     mail.extend(additional_mail)
     mail.extend(techmails_list)
+    logger.info("helpers::get_peer_techc_mails(): additional_mail="+str(additional_mail))
+    logger.info("helpers::get_peer_techc_mails(): techmails_list="+str(techmails_list))
     return mail
