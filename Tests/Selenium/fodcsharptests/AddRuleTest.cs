01e05295292fd738b7cb9e297a8710b958db139d
Ecaterina Matenco <ematenco@galex.md>
2019-09-20 17:05:05 +0300

    added logout tests

--- Tests/Selenium/fodcsharptests/AddRuleTest.cs
+++ Tests/Selenium/fodcsharptests/AddRuleTest.cs
@@ -20,7 +20,7 @@ namespace fodcsharptests
         {
             driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            driver.Url = "http://localhost:8083/altlogin";
+            
         }
 
 
@@ -28,7 +28,8 @@ namespace fodcsharptests
         [Test]
         public void AddName()
         {
-           
+            driver.Url = "http://localhost:8083/altlogin";
+
             driver.FindElement(By.Id("id_username")).Click();
             driver.FindElement(By.Id("id_username")).SendKeys("admin");
             driver.FindElement(By.Id("id_password")).Click();
@@ -38,6 +39,9 @@ namespace fodcsharptests
             driver.FindElement(By.Id("applybutton")).Click();
             wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
 
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
             driver.FindElement(By.Id("id_name")).Click();
             driver.FindElement(By.Id("id_name")).SendKeys("npattack");
 
@@ -45,10 +49,241 @@ namespace fodcsharptests
             driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
 
             driver.FindElement(By.Id("id_destination")).Click();
-            driver.FindElement(By.Id("id_destination")).SendKeys("1");
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            //What should happend? 
         }
 
+        [Test]
+        public void AddWrongName()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
 
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("1' or '1' = '1 /*");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'Enter a valid 'slug' consisting of letters, numbers, underscores or hyphens.')]"));
+        }
+
+        [Test]
+        public void AddWrongSourceAddress()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("npattack");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("efefef");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'Invalid network address format')]"));
+        }
+
+        [Test]
+        public void AddWrongDestinationAddress()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("npattack");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("drgr");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'Invalid network address format')]"));
+        }
+
+        [Test]
+        public void AddWithOutExpires()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("npattack");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.Id("id_expires")).Click();
+            driver.FindElement(By.Id("id_expires")).Clear();
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'This field is required.')]"));
+        }
+
+        [Test]
+        public void AddWrongSrcPort()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("npattack");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.Id("id_sourceport")).Click();
+            driver.FindElement(By.Id("id_sourceport")).SendKeys("f//");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'Malformed port range format, example: 80,1000-1100,6000-6010')]"));
+        }
+
+        [Test]
+        public void AddWrongDestPort()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("npattack");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.Id("id_destinationport")).Click();
+            driver.FindElement(By.Id("id_destinationport")).SendKeys("f//");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'Malformed port range format, example: 80,1000-1100,6000-6010')]"));
+        }
+
+        [Test]
+        public void AddWrongPort()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+
+            driver.FindElement(By.Id("routebutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("Apply for a new rule"));
+
+            driver.FindElement(By.Id("id_name")).Click();
+            driver.FindElement(By.Id("id_name")).SendKeys("npattack");
+
+            driver.FindElement(By.Id("id_source")).Click();
+            driver.FindElement(By.Id("id_source")).SendKeys("0.0.0.0/0");
+
+            driver.FindElement(By.Id("id_destination")).Click();
+            driver.FindElement(By.Id("id_destination")).SendKeys("0.0.0.0/29");
+
+            driver.FindElement(By.Id("id_port")).Click();
+            driver.FindElement(By.Id("id_port")).SendKeys("f//");
+
+            driver.FindElement(By.LinkText("Apply")).Click();
+
+            driver.FindElement(By.XPath("//*[contains(text(), 'Malformed port range format, example: 80,1000-1100,6000-6010')]"));
+        }
 
         [TearDown]
 
---
+++ Tests/Selenium/fodcsharptests/SetupTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace fodcsharptests
+{
+    [TestFixture]
+    public class SetupTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        
+
+        [Test]
+        public void SuccessLogin()
+        {
+            driver.Url = "http://localhost:8083/altlogin";
+
+            driver.FindElement(By.Id("id_username")).Click();
+            driver.FindElement(By.Id("id_username")).SendKeys("admin");
+            driver.FindElement(By.Id("id_password")).Click();
+            #region MyPasswordIsHere
+            driver.FindElement(By.Id("id_password")).SendKeys("1");
+            #endregion
+            driver.FindElement(By.Id("applybutton")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("GÉANT OC FoD :: My rules"));
+        }
+
+
+        [TearDown]
+
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
