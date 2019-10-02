dc22ab4bcea7fd7347ea90a0eaad74655721ae86
Tomas Cejka <cejkat@cesnet.cz>
2019-10-02 11:32:06 +0000

    Updated runfod.sh to start new celery
    Removed python2.6 workaround, and beanstalkd startup

--- runfod.sh
+++ runfod.sh
@@ -14,28 +14,11 @@ fi
 
 ##
 
-# fix needed to make sure gevent works # may be put into Dockerfile instead
-if grep ssl_version=PROTOCOL_SSLv3 /srv/venv/lib/python2.7/site-packages/gevent/ssl.py; then
-   cat /srv/venv/lib/python2.7/site-packages/gevent/ssl.py | \
-	sed -e 's/ssl_version=PROTOCOL_SSLv3/ssl_version=PROTOCOL_SSLv23/' > /srv/venv/lib/python2.7/site-packages/gevent/ssl.py.$$ && \
-	mv -vf /srv/venv/lib/python2.7/site-packages/gevent/ssl.py.$$ /srv/venv/lib/python2.7/site-packages/gevent/ssl.py || \
-	rm -vf /srv/venv/lib/python2.7/site-packages/gevent/ssl.py.$$
-fi
-
-##
-
 cd "$(dirname "$0")"
-
-echo "Starting services FoD is depending on generically: beanstalkd" 1>&2
-if which service; then
-  service beanstalkd start
-else
-  beanstalkd &
-fi
 
 if [ ! -x ./fodcli_db_is_mysql ] || ./fodcli_db_is_mysql; then
   echo "Starting DB services FoD might be depending on (depending on its config): mysql" 1>&2
-  service mysql start
+  systemctl start mysql.service
 fi
 
 # hook to initiallize (without any user interaction admin user and its peer data)
@@ -44,8 +27,7 @@ fi
 mkdir -p /var/run/fod
 
 echo "Starting FoD celeryd in background" 1>&2
-#pgrep -f "python.*celeryd"  || ./manage.py celeryd &
-pgrep -f "python.*celeryd" >/dev/null  || { ./manage.py celeryd -E --soft-time-limit=180 --concurrency=1 -B --time-limit=1800 & }
+celery worker -A flowspy --concurrency=2 --detach -l info
 
 echo "Starting FoD gunicorn in foreground" 1>&2
 #exec ./manage.py runserver 0.0.0.0:8000 
