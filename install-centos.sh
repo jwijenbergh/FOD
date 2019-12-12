9b955a319c32626cdfb05d85afbeaea7e4b3f93c
schmitz <schmitz@lrz.de>
2019-12-12 09:39:42 +0100

    some fixes for centos dockerfile

--- install-centos.sh
+++ install-centos.sh
@@ -4,11 +4,13 @@
 # with Celery, Redis, and sqlite.
 #
 
+set -e
+
 echo "Installing epel repo"
 rpm -Uh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 echo "Installing remi repo"
-yum -q -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
 echo "Installing base dependencies"
 yum -y install python36 python3-setuptools python36-virtualenv vim git gcc libevent-devel libxml2-devel libxslt-devel mariadb-server mysql-devel patch yum-utils
@@ -23,23 +25,26 @@ echo "Setup python environment for FoD"
 mkdir -p /var/log/fod /srv
 virtualenv-3 /srv/venv
 (
+        set +e
 	source /srv/venv/bin/activate
 	mkdir -p /srv/flowspy/
 
 	# Select source dir and copy FoD into /srv/flowspy/
 	if [ "`basename "$0"`" = install-centos.sh ]; then
 		# this script is in the source directory
-		cp -r "`dirname $0`"/* /srv/flowspy/
+		cp -f -r "`dirname $0`"/* /srv/flowspy/
 	elif [ -e /vagrant ]; then
 		# vagrant's copy in /vagrant/
-		cp -r /vagrant/* /srv/flowspy/
+		cp -f -r /vagrant/* /srv/flowspy/
 	elif [ -e ./install-centos.sh ]; then
 		# current directory is with the sourcecode
-		cp -r ./* /srv/flowspy/
+		cp -f -r ./* /srv/flowspy/
 	else
 		echo "Could not find FoD src directory tried `dirname $0`, /vagrant/, ./"
 		exit 1
 	fi
+
+	set -e
 	
 	cd /srv/flowspy/
 	(
@@ -51,7 +56,7 @@ virtualenv-3 /srv/venv
 
 	touch flowspy/settings_local.py
 
-	./manage.py syncdb --noinput
+	#./manage.py syncdb --noinput
 	./manage.py migrate
 	./manage.py loaddata initial_data
 )
