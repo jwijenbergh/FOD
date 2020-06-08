34a51469bd5d365a7045c2f700a1ca4b9ade5d4f
Tomas Cejka <cejkat@cesnet.cz>
2020-06-08 18:01:10 +0000

    celery: start periodic tasks

--- flowspy/celery.py
+++ flowspy/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from flowspy import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowspy.settings')
@@ -16,6 +17,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = settings.CELERYBEAT_SCHEDULE
 
 @app.task(bind=True)
 def debug_task(self):
