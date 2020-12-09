71702ec30a3b45c4fbfbfb8eb49b924cbf39fc36
David Schmitz <schmitz@lrz.de>
2020-12-09 14:06:19 +0100

    update for docker build

--- install-centos.sh
+++ install-centos.sh
@@ -95,6 +95,7 @@ else
 	touch flowspy/settings_local.py
 
 	#./manage.py syncdb --noinput
+	mkdir -p static
 	echo yes | ./manage.py collectstatic
 	./manage.py migrate
 	./manage.py loaddata initial_data
