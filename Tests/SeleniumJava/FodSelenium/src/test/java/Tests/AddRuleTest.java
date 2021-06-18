5d57a148e5c3cd582b64d9877a19dccfd5d356c2
Ecaterina Matenco <ecaterina.matenco@gmail.com>
2021-06-18 14:32:43 +0300

    package name fix

--- Tests/SeleniumJava/FodSelenium/src/test/java/Tests/AddRuleTest.java
+++ Tests/SeleniumJava/FodSelenium/src/test/java/Tests/AddRuleTest.java
@@ -1,4 +1,4 @@
-package Tests;
+package test.src.Tests;
 
 import java.io.FileWriter;
 import java.io.IOException;
--- Tests/SeleniumJava/FodSelenium/src/test/java/Tests/DashboardTest.java
+++ Tests/SeleniumJava/FodSelenium/src/test/java/Tests/DashboardTest.java
@@ -1,4 +1,4 @@
-package Tests;
+package test.java.Tests;
 
 import java.io.FileWriter;
 import java.io.IOException;
