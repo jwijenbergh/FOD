ed9494c041af2b9ae7b1f9c3818a3a185335a001
David Schmitz <schmitz@lrz.de>
2022-08-08 06:51:31 +0000

    fix broken data table reload mechanism

--- templates/pollerdash.js
+++ templates/pollerdash.js
@@ -18,10 +18,10 @@ function getCookie(name) {
 
 jQuery.postJSON = function(url, args, callback) {
     $.ajax({url: url, dataType: "json", type: "POST", cache: false,
-	    success: function(response) {
-	if (callback) callback(response);
+            success: function(response) {
+        if (callback) callback(response);
     }, error: function(response) {
-	console.log("ERROR:", response);
+        console.log("ERROR:", response);
     }});
 };
 
@@ -29,7 +29,7 @@ jQuery.fn.formToDict = function() {
     var fields = this.serializeArray();
     var json = {}
     for (var i = 0; i < fields.length; i++) {
-	json[fields[i].name] = fields[i].value;
+        json[fields[i].name] = fields[i].value;
     }
     if (json.next) delete json.next;
     return json;
@@ -49,47 +49,72 @@ jQuery.fn.enable = function(opt_enable) {
     return this;
 };
 
+
+var dataTableUpdate_lock = false;
+function reloadDataTable(cause) {
+
+  console.log("reloadDataTable(): cause="+cause+" dataTableUpdate_lock="+dataTableUpdate_lock);
+
+  if (!dataTableUpdate_lock) {
+    dataTableUpdate_lock = true
+
+    try {
+      console.log("DataTable reload triggered after new rule update messages");
+
+      //oTable.fnReloadAjax(refreshUrl);
+      //console.log("oTable="+oTable);
+      //console.log("ajax2="+$('#routes_table').DataTable().ajax);
+      $('#routes_table').DataTable().ajax.reload();
+
+      console.log("DataTable reload returned");
+    } catch (e) {
+      console.log("DataTable reload failed: "+e);
+    }
+
+    dataTableUpdate_lock = false
+  }
+}
+
+
 var updater = {
     started: false,
     last_ids: new Object(),
     start: function() {
-	    //console.log("Initial fetching of all notifications.");
-	    {% for peer in user.userprofile.peers.all %}
-	    updater.last_ids["{{ peer.pk }}"] = null;
+            //console.log("Initial fetching of all notifications.");
+            {% for peer in user.userprofile.peers.all %}
+            updater.last_ids["{{ peer.pk }}"] = null;
         try {
-	    $.ajax({url: "{% url 'fetch-existing'  peer.pk %}", type: "POST", dataType: "json", cache:false,
-			    success: updater.onFetchExisting,
-			    error: updater.onError});
+            $.ajax({url: "{% url 'fetch-existing'  peer.pk %}", type: "POST", dataType: "json", cache:false,
+                            success: updater.onFetchExisting,
+                            error: updater.onError});
         } catch (e) {
             console.log("Error: " + e);
         }
-	    {% endfor %}
+            {% endfor %}
     },
     poll: function() {
-    	{% if user.is_authenticated %}
-    	timeout = {{timeout}};
-        {% for peer in user.userprofile.peers.all %}
-        //console.log("Polling new notifications from", updater.last_ids["{{ peer.pk }}"], "peerid {{ peer.pk }}");
-        $.ajax({url: "{% url 'fetch-updates'  peer.pk 'PLACEHOLDER' %}".replace("PLACEHOLDER", updater.last_ids["{{ peer.pk }}"]), type: "POST", dataType: "json", cache:false,
-    		success: updater.onSuccess,
-    		timeout: timeout,
-    		error: updater.onError});
-        {% endfor %}
-    	{% endif %}
+        {% if user.is_authenticated %}
+          timeout = {{timeout}};
+          {% for peer in user.userprofile.peers.all %}
+           //console.log("Polling new notifications from", updater.last_ids["{{ peer.pk }}"], "peerid {{ peer.pk }}");
+           $.ajax({url: "{% url 'fetch-updates'  peer.pk 'PLACEHOLDER' %}".replace("PLACEHOLDER", updater.last_ids["{{ peer.pk }}"]), type: "POST", dataType: "json", cache:false,
+                success: updater.onSuccess,
+                timeout: timeout,
+                error: updater.onError});
+          {% endfor %}
+        {% endif %}
     },
     onSuccess: function(response) {
         try {
-            updater.newMessages(response);
-            var errdialog = $('#ajaxerror')
-            if (errdialog[0]) {
-                errdialog[0].remove();
-            }
-            if (oTable) {
-                try {
-                    oTable.fnReloadAjax(refreshUrl);
-                } catch (e) {
-                    console.log("DataTable reload failed.");
-                }
+            if (response!=undefined) {
+              updater.newMessages(response);
+              var errdialog = $('#ajaxerror')
+              if (errdialog[0]) {
+                  errdialog[0].remove();
+              }
+              /*if ($('#routes_table')) {
+                reloadDataTable("on success response="+response);
+              }*/
             }
         } catch (e) {
             console.log(e);
@@ -98,17 +123,17 @@ var updater = {
     },
 
     onFetchExisting: function(response) {
-    	try {
-    	    updater.existingMessages(response);
-    	    if (updater.started == false) {
+        try {
+            updater.existingMessages(response);
+            if (updater.started == false) {
                 updater.started = true;
                 setInterval(updater.poll, 3000);
             }
-    	} catch (e) {
+        } catch (e) {
             console.log(e);
-    	    updater.onError();
-    	    return;
-    	}
+            updater.onError();
+            return;
+        }
     },
 
     onError: function(response, text) {
@@ -120,82 +145,99 @@ var updater = {
     },
 
     newMessages: function(response) {
-        if (!response || !response.messages) return;
+
+        //console.log("newMessages(): response="+response)
+        //console.log("newMessages(): response.messages="+response.messages)
+
+        if (!response || !response.messages) {
+            //console.log("newMessages(): !response or !messages")
+            return;
+        }
         if (response.messages.length == 0){
+            //console.log("newMessages(): 0 messages")
             return true;
         }
+        
+        console.log("newMessages: inner");
+
         var messages = response.messages;
         var peerid = messages[messages.length - 1]["peerid"];
         var msgid = messages[messages.length - 1]["id"];
         updater.last_ids[peerid] = msgid;
-        //console.log(messages.length, "new messages, last_id:", msgid, "peerid", peerid);
+
+        console.log(messages.length, "new messages, last_id:", msgid, "peerid", peerid);
 
         var reloadContent = false;
         for (var i = 0; i < messages.length; i++) {
-            updater.showMessage(messages[i]);
-            if (messages[i].body.match(/Successfully committed$/)) {
+            updater.showMessage(messages[i], peerid);
+            try {
+              body = messages[i].body 
+              if (body.match(/Successfully committed$/) || body.match(/Deleting inactive/)) {
                 reloadContent = true;
+              }
+            } catch (e) {
+              console.log("message processing: "+e);
             }
         }
         $("#hid_mid").val('UPDATED');
         if (reloadContent) {
-            if (oTable) {
-                try {
-                    oTable.fnReloadAjax(refreshUrl);
-                } catch (e) {
-                    console.log("DataTable reload failed.");
-                }
-            } else {
-                location.reload();
+            if ($('#routes_table')) {
+              reloadDataTable("on messages.length="+messages.length);
             }
         }
     },
 
     existingMessages: function(response) {
-    	if (!response || !response.messages) return;
-    	if (response.messages.length == 0){
+        if (!response || !response.messages) return;
+        if (response.messages.length == 0){
             return true;
-    	}
+        }
         $("#inbox").empty();
-    	var messages = response.messages;
+        var messages = response.messages;
         var peerid = messages[messages.length - 1]["peerid"];
         var msgid = messages[messages.length - 1]["id"];
         updater.last_ids[peerid] = msgid;
         //console.log("got", messages.length, "messages with last_id", msgid);
-    	for (var i = 0; i < messages.length; i++) {
-    	    updater.showMessage(messages[i]);
-    	}
+        for (var i = 0; i < messages.length; i++) {
+            updater.showMessage(messages[i], peerid);
+        }
     },
 
-    showMessage: function(message) {
+    showMessage: function(message, peerid) {
         var existing = $("#m" + message.id);
         if (existing.length > 0) return;
-        var username = message.body.split("]")[0].replace("[","");
-        var mbody = message.body.replace("["+username+"] ","");
-        var htmlnode = '<li class="left clearfix">\
-                                    <div class="chat-body clearfix" style="margin-left: 0px;"> \
-                                        <div class="header"> \
-                                            <small class="pull-right text-muted"> \
-                                                <i class="fa fa-clock-o fa-fw"></i> '+ message.time +'  \
-                                            </small>\
-                                        </div>\
-                                        <p><small><strong class="primary-font">'+username+'</strong>:\
-                                            '+ mbody+'\
-                                        </small></p>\
-                                    </div>\
-                                </li>';
-        var node = $(htmlnode);
-        node.hide();
-        $("#inbox").prepend(node);
-        node.slideDown();
+
+        try {
+          var username = message.body.split("]")[0].replace("[","");
+          //var mbody = message.body.replace("["+username+"] ","");
+          var mbody = message.body.replace("["+username+"] ", "["+peerid+"] ");
+          var htmlnode = '<li class="left clearfix">\
+                                      <div class="chat-body clearfix" style="margin-left: 0px;"> \
+                                          <div class="header"> \
+                                              <small class="pull-right text-muted"> \
+                                                  <i class="fa fa-clock-o fa-fw"></i> '+ message.time +'  \
+                                              </small>\
+                                          </div>\
+                                          <p><small><strong class="primary-font">'+username+'</strong>:\
+                                              '+ mbody+'\
+                                          </small></p>\
+                                      </div>\
+                                  </li>';
+          var node = $(htmlnode);
+          node.hide();
+          $("#inbox").prepend(node);
+          node.slideDown();
+        } catch (e) {
+          console.log("showMessage(): "+e)
+        }
     }
 };
 
 function blink(selector){
-	$(selector).animate({color: "#EE5F5B"}, 500, function(){
-		$(this).animate({ color: "white" }, 500, function(){
-			blink(this);
-		});
-	});
+        $(selector).animate({color: "#EE5F5B"}, 500, function(){
+                $(this).animate({ color: "white" }, 500, function(){
+                        blink(this);
+                });
+        });
 }
 
