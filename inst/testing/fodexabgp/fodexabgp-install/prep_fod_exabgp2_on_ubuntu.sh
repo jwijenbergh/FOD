376869cb585e93d01d65735ca3470a49b552bd62
David Schmitz <schmitz@lrz.de>
2023-04-25 09:45:33 +0000

    update for ./inst/testing/fodexabgp

--- inst/testing/fodexabgp/fodexabgp-install/prep_fod_exabgp2_on_ubuntu.sh
+++ inst/testing/fodexabgp/fodexabgp-install/prep_fod_exabgp2_on_ubuntu.sh
@@ -3,21 +3,22 @@
 set -e
 set -x
 
-#mkdir -p /opt
-#cd /opt
-#
-#if [ ! -d "./FOD" ]; then
-#  git clone https://github.com/GEANT/FOD    
-#fi
-#
-#cd FOD
-#
-#git checkout refs/remotes/origin/feature/exabgp_support2
-#git checkout -b feature/exabgp_support2
+mkdir -p /opt
+cd /opt
+
+if [ ! -d "./FOD" ]; then
+  git clone https://github.com/GEANT/FOD    
+fi
+
+cd FOD
+
+git checkout refs/remotes/origin/feature/exabgp_support2
+git checkout -b feature/exabgp_support2
 
 #./install-debian.sh --here # make sure user fod has access right to /opt/FOD
 #./install-debian.sh --here --setup_admin_user --supervisord --setup_admin_user5 admin adminpwd admin@localhost.local testpeer 0.0.0.0/0 # make sure user fod has access right to /opt/FOD
-./install-debian.sh --git-checkout https://github.com/GEANT/FOD feature/exabgp_support2 /opt/FOD --here --setup_admin_user --setup_admin_user5 admin adminpwd admin@localhost.local testpeer 0.0.0.0/0 --exabgp0 "$@"
+#./install-debian.sh --git-checkout https://github.com/GEANT/FOD feature/exabgp_support2 /opt/FOD --here --setup_admin_user --setup_admin_user5 admin adminpwd admin@localhost.local testpeer 0.0.0.0/0 --exabgp0 "$@"
+./install-debian.sh --here --setup_admin_user --setup_admin_user5 admin adminpwd admin@localhost.local testpeer 0.0.0.0/0 --exabgp0 "$@"
 
 #chown -R fod: /opt/FOD/ # fix maybe wrong permissions # not necessary anymore 
 
