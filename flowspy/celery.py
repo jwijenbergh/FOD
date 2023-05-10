dea0aead8e461ccd2195804fd82bd895a42c562e
David Schmitz <schmitz@lrz.de>
2023-05-10 12:17:54 +0000

    cleanup of left-over lock files at celery startup

--- flowspy/celery.py
+++ flowspy/celery.py
@@ -2,6 +2,13 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from flowspy import settings
+import sys
+
+##
+
+print("celery app start", file=sys.stderr)
+
+##
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowspy.settings')
@@ -13,6 +20,23 @@ app = Celery('flowspy')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+##
+
+if hasattr(settings, 'SNMP_POLL_LOCK'):
+    SNMP_POLL_LOCK=settings.SNMP_POLL_LOCK
+    print("SNMP_POLL_LOCK="+str(SNMP_POLL_LOCK))
+    if SNMP_POLL_LOCK!='' and os.path.exists(SNMP_POLL_LOCK):
+      print("trying to remove "+str(SNMP_POLL_LOCK), file=sys.stderr)
+      os.rmdir(SNMP_POLL_LOCK)
+
+    SNMP_TEMP_FILE=settings.SNMP_TEMP_FILE
+    print("SNMP_TEMP_FILE="+str(SNMP_TEMP_FILE))
+    if SNMP_TEMP_FILE!='' and os.path.exists(SNMP_TEMP_FILE+'.lock'):
+      print("trying to remove "+str(SNMP_TEMP_FILE+'.lock'), file=sys.stderr)
+      os.rmdir(settings.SNMP_TEMP_FILE+'.lock')
+
+##
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
