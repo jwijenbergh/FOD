3b2c577b8e52676708bf756ea85c753f3f073f56
David Schmitz <schmitz@lrz.de>
2023-05-19 09:46:58 +0000

    fix/ipv6_rules_implicit_icmp6: add setting NETCONF_FLOWSPEC_MAP_ICMP_TO_ICMPV6 to enable/disable implicit ipv6 addresses+proto=icmp <-> proto=icmpv6

--- utils/flowspec_utils.py
+++ utils/flowspec_utils.py
@@ -7,14 +7,16 @@ logger = flowspec.logging_utils.logger_init_default(__name__, "flowspec_utils.lo
 def map__ip_proto__for__ip_version__to_flowspec(ip_version, protocol_str):
     logger.info("map__ip_proto__for__ip_version__to_flowspec(): called ip_version="+str(ip_version)+" protocol_str="+str(protocol_str))
     if ip_version==6 and protocol_str=='icmp': 
-        protocol_str='icmp6'
+        if hasattr(settings, 'NETCONF_FLOWSPEC_MAP_ICMP_TO_ICMPV6') and settings.NETCONF_FLOWSPEC_MAP_ICMP_TO_ICMPV6:
+          protocol_str='icmp6'
     logger.info("map__ip_proto__for__ip_version__to_flowspec(): returning protocol_str="+str(protocol_str))
     return protocol_str
 
 def map__ip_proto__for__ip_version__from_flowspec(ip_version, protocol_str):
     logger.info("map__ip_proto__for__ip_version__from_flowspec(): called ip_version="+str(ip_version)+" protocol_str="+str(protocol_str))
     if ip_version==6 and protocol_str=='icmp6': 
-        protocol_str='icmp'
+        if hasattr(settings, 'NETCONF_FLOWSPEC_MAP_ICMP_TO_ICMPV6') and settings.NETCONF_FLOWSPEC_MAP_ICMP_TO_ICMPV6:
+          protocol_str='icmp'
     logger.info("map__ip_proto__for__ip_version__from_flowspec(): returning protocol_str="+str(protocol_str))
     return protocol_str
 
